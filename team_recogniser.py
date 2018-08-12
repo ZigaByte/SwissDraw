c5930f91b3fdfdde039f66c16d2dd6626c55c6fc
ZigaByte <zvene28@gmail.com>
2018-08-12 18:00:36 +0200

    Program now reads the incidence matrix and the scores difference. Returns in a pair.

--- team_recogniser.py
+++ team_recogniser.py
@@ -1,8 +1,54 @@
 # The idea of this program is to open an excel file and read
 # the different team names from column A and column B
-
 import pandas
 
+# Returns a pair with:
+# - 2D incidence matrix for matches in the list
+# - vector with difference in results
+def generateIncidenceMatrix(file_name):
+    data = pandas.read_excel(file_name, "swiss_matches", None)
+    matches = data.to_dict("index")
+
+    team_index = getTeamDictionary(file_name)
+    team_count = len(team_index)
+
+    # What we fill in
+    A = [] 
+    d = []
+
+    print("Team Dict.:")
+    print(team_index)
+    print()
+    
+    for match in matches:
+        team1 = matches[match][0]
+        team2 = matches[match][1]
+
+        # Fill the row of this match
+        a = []
+        for i in range(team_count):
+            if(i == team_index[team1]):
+                a.append(1)
+            elif(i == team_index[team2]):
+                a.append(-1)
+            else:
+                a.append(0)
+
+        # Add this match's row to the matrix
+        A.append(a)
+
+        d.append(matches[match][2] - matches[match][3])
+
+        '''
+        print("Match %d" % match)
+        print(team1)
+        print(team2)
+        print(a)
+        print()
+        '''
+    return (A, d)
+    
+    
 # Returns a dictionary of teams. Format: {'Team Name': index, ...}
 def getTeamDictionary(file_name):
     data = pandas.read_excel(file_name, "teams", None)
@@ -16,4 +62,5 @@ def getTeamDictionary(file_name):
 
     return teams
 
-print(getTeamDictionary("swiss_draw.xlsx"))
+#print(getTeamDictionary("swiss_draw.xlsx"))
+print(generateIncidenceMatrix("swiss_draw.xlsx"))
